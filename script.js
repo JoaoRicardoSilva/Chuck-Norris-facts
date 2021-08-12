721fb770687039f6be2ce39189817fcfdc4fb8cf
João Ricardo <joaoricardopratasilva@hotmail.com>
2021-08-12 23:13:39 +0100

    make responsive

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 const fact = $("#fact");
 
 const fetchFact = async () => {
-    try {
-        const response = await fetch(
-            "https://api.chucknorris.io/jokes/random/"
-        );
-        const jsonResp = await response.json();
+  try {
+    const response = await fetch("https://api.chucknorris.io/jokes/random/");
+    const jsonResp = await response.json();
 
-        return jsonResp.value;
-    } catch (error) {
-        console.log(error);
-    }
-    return;
+    return jsonResp.value;
+  } catch (error) {
+    console.log(error);
+  }
+  return;
 };
 
 const getFact = () => {
-    fetchFact().then((str) => {
-        fact.text(str);
-    });
+  fetchFact().then((str) => {
+    fact.text(str);
+  });
 };
 
 getFact();
 
 $(".btn").click(() => {
-    fact.empty();
-    getFact();
+  fact.empty();
+  getFact();
 });
